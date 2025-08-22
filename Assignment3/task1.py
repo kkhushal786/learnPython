daf980bb2a8748d1954423d46fd36df37d55ec7a
kushalmosaic <kushals@mosaic-apps.com>
2025-08-23 01:50:51 +0530

    submit assignment 3

---
+++ Assignment3/task1.py
@@ -0,0 +1,6 @@
+from math import factorial
+
+a = input("Enter a number: ")
+b = factorial(int(a));
+
+print("factorial of", a, "is:", b)
---
+++ Assignment3/task2.py
@@ -0,0 +1,9 @@
+import math
+
+a = input("Enter a number: ")
+b = math.sqrt(int(a))
+print("Square root:", b)
+c = math.log(int(a))
+print("Logarithm:", c)
+d = math.sin(int(a))
+print("Sine:", d)
